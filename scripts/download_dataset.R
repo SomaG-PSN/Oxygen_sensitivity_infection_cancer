6709cb913ce2129faf0ed05fcf6f7844df92acc4
SomaG-PSN <somag829@gmail.com>
2025-08-05 23:46:53 -0400

    Data downloaded

--- scripts/download_dataset.R
+++ scripts/download_dataset.R
@@ -13,6 +13,18 @@ stad_datasets <- XenaData %>%
   filter(str_detect(XenaCohorts, "TCGA Stomach Cancer"))
 
 #Generate Queries
-query_expr <- XenaGenerate(
-  dataset = ""
-)
+query_expr <- XenaGenerate(subset = XenaDatasets == "TCGA.STAD.sampleMap/HiSeqV2_PANCAN")
+query_clin <- XenaGenerate(subset = XenaDatasets == "TCGA.STAD.sampleMap/STAD_clinicalMatrix")
+
+#Download and prepare data
+
+expr_data <- XenaQuery(query_expr) %>%
+  XenaDownload() %>%
+  XenaPrepare()
+
+clinical_data <- XenaQuery(query_clin) %>%
+  XenaDownload() %>%
+  XenaPrepare()
+
+write.csv(expr_data, "../results/TCGA_STAD_expr_PANCAN.csv")
+write.csv(clinical_data, "../results/TCGA_STAD_clinical.csv")
